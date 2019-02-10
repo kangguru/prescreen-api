10424cb07c95697131a5a4b99d512002714de3a3
Johannes Heck <yoyostile@gmail.com>
2019-02-10 19:40:09 +0100

    even more magic

--- lib/prescreen/api/candidate.rb
+++ lib/prescreen/api/candidate.rb
@@ -3,6 +3,15 @@
 module Prescreen
   module Api
     class Candidate < Base
+      def applications
+        Prescreen::Api::Application.find(:all, params: { candidate_id: id })
+      end
+
+      class << self
+        def find_by(opts = {})
+          find(:all, params: opts)
+        end
+      end
     end
   end
 end
